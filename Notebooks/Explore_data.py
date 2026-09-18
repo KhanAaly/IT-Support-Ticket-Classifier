0cbd3396401849df2814021bfbabbb87c78e12da
Aaly Khan <2coolaaly@gmail.com>
2026-09-17 20:36:52 -0400

    Building regression model

--- Notebooks/Explore_data.py
+++ Notebooks/Explore_data.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
 
 df = pd.read_csv('Data/support_tickets_sample.csv')
 
@@ -14,9 +15,9 @@ print(df.columns)
 print(df.isnull().sum())
 print(df.duplicated().sum())
 category_counts = df["category"].value_counts()
-print(df["subject"].head)
-print(df["description"].head)
-print(df["category"].head)
+print(df["subject"].head())
+print(df["description"].head())
+print(df["category"].head())
 
 plt.bar(category_counts.index, category_counts.values)
 plt.xticks(rotation=45)
@@ -35,7 +36,17 @@ x_train, x_test, y_train, y_test = train_test_split(
     stratify=y
 )
 
-print(y_train.value_counts())
+x_train_test = x_train["subject"] + " " + x_train["description"]
+x_test_test = x_test["subject"] + " " + x_test["description"]
+
+vectorizer = TfidfVectorizer()
+vectorizer.fit(x_train_test)
+x_train_tfidf = vectorizer.transform(x_train_test)
+x_test_tfidf = vectorizer.transform(x_test_test)
+model = LogisticRegression(max_iter=1000)
+model.fit(x_train_tfidf, y_train)
+model_predictions = model.predict(x_test_tfidf)
+
 
 most_common_category = y_train.value_counts().index[0]
 
@@ -43,4 +54,7 @@ most_common_category = y_train.value_counts().index[0]
 baseline_predictions = np.full(len(y_test), most_common_category)
 baseline_accuracy = accuracy_score(y_test, baseline_predictions)
 print("baseline accuracy:", baseline_accuracy)
+
+model_accuracy = accuracy_score(y_test, model_predictions)
+print("model accuracy:", model_accuracy)
 #plt.show()
